aa6249ac097cceac1b11e3e9cd98e550cb033a70
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-04-12 11:40:58 -0300

    fix: removing  unused code

--- src/components/ThemeSwitcher/index.tsx
+++ src/components/ThemeSwitcher/index.tsx
@@ -1,40 +1,21 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback} from 'react';
 import Switch from 'react-switch';
 import useTheme from '../../hooks/useTheme';
 import { shade } from 'polished';
-import { IoMdMoon as Moon, IoMdSunny as Sun, IoMdBulb as Bulb } from 'react-icons/io';
-import { RiLightbulbFlashLine as BulbActive} from 'react-icons/ri';
-
-import light from '../../styles/themes/light';
-import dark from '../../styles/themes/dark';
+import { RiLightbulbFlashLine as Bulb} from 'react-icons/ri';
 
 import { Container } from "./styles";
 
 const ThemeSwitcher: React.FC = () => {
-  const [isChecked, setChecked] = useState(false);
-
   const {theme, toggleTheme} = useTheme()
 
-  useEffect(() => {
-
-  },[])
   const handleSwitched = useCallback(() => {
     toggleTheme();
-    // setChecked((oldChecked: boolean) => !oldChecked)
   }, [toggleTheme])
-
-  // const [theme, setTheme] = useState();
-
-  // const
-
-  // const { colors } = useContext(ThemeContext);
-
-
-
 
   return (
     <Container title="Deslize para o modo dark ou light">
-      <BulbActive size={25} className={`icon ${theme.title === 'light' ? 'active' : ''}`}/>
+      <Bulb size={25} className={`icon ${theme.title === 'light' ? 'active' : ''}`}/>
       <Switch
         onChange={handleSwitched}
         checked={theme.title === 'dark' ? true : false}
@@ -47,7 +28,6 @@ const ThemeSwitcher: React.FC = () => {
         onColor={theme.colors.active}
 
       />
-      {/* <Bulb className={`icon ${theme.title === 'dark' ? 'active' : ''}`}/> */}
     </Container>
   )
 }
